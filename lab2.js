68005912d1432701e319bc8cbc6370d6ab22df8d
danielgbc7 <daniel.konjarski@georgebrown.ca>
2024-09-10 18:32:56 -0400

    lab2.1

---
+++ lab2.js
@@ -0,0 +1,30 @@
+var add = function (a, b){
+    return a+b;
+}
+
+var sub = function (a, b){
+    return a-b;
+}
+
+var makeACall = function (a, b, add, sub){
+    if(a>b)
+        return sub(a, b);
+    else
+        return add(a, b);
+}
+
+var r = makeACall(5, 3, add, sub);
+console.log(r);
+
+var print = function () {
+    console.log("Hello-1")
+}
+
+setInterval(print, 1000)
+
+setInterval(() => {
+    console.log("Hello-2");
+}, 2000);
+
+////////////////////////////////
+
